e288bc6f58d6291f2dfd360a86ca046062acf3ac
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-02-10 14:30:50 +0530

    arr_vector

--- 10.02.2026/2nd.cpp
+++ 10.02.2026/2nd.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 int main()
 {
-    vector<int>a={1,2,3,4,5};
+    vector<int>a={1,2,8,4,5};
     for( int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
     cout<<endl;
     cout<<a.front()<<endl;
     cout<<a.back()<<endl;
+     sort(a.begin(), a.end());
+    reverse(a.begin(),a.end());
+   
+    a.insert(a.end()-1,6);
+    for( int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
 return 0;
 }
