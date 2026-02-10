c5efc3128ea19f2ea43cd259d6681fd126c80220
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-02-10 14:13:00 +0530

    vector

---
+++ 10.02.2026/2nd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>a={1,2,3,4,5};
+    for( int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<a.front()<<endl;
+    cout<<a.back()<<endl;
+return 0;
+}
