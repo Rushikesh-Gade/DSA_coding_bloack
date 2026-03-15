1828c7213138019dacbcec661004477947cf4f39
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-03-15 23:07:41 +0530

    .

---
+++ assigment-5/Column_with_maximum_sum_in_a_Matrix.cpp
@@ -0,0 +1,61 @@
+// Given a N*N matrix. The task is to find the index of column with maximum sum. That is the column whose sum of elements are maximum.
+
+
+// Input Format
+// First line contains the N ,size of the square matrix. Next N lines contains N integers each denoting the elements of the matrix
+
+
+// Constraints
+// 1<=N<1000
+
+
+// Output Format
+// Print two integers: the 1-based index of the column with the maximum sum and the maximum sum itself.
+
+
+// Sample Input
+// 7
+// 90 40 1 3 39 59 90 
+// 48 72 67 32 73 19 27 
+// 22 37 47 68 1 5 55 
+// 81 5 39 53 38 86 21 
+// 1 32 7 44 2 65 47 
+// 68 13 24 28 69 81 43 
+// 16 34 67 3 82 26 35 
+// Sample Output
+// 6 341
+// Explanation
+// 6th column has the highest sum that is 341.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main () {
+	int n;
+	cin>>n;
+	vector<vector<int>> a(n, vector<int>(n));
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cin>>a[i][j];
+		}
+	}
+	int ans=0, sum=0, m, c;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			// cout<<a[j][i]<<" ";
+			sum+=a[j][i];
+			m=i+1;
+		}
+		// cout<<endl;
+	if(ans<sum)
+	{
+		ans=sum;
+		c=m;
+	}
+
+	sum=0;
+	}
+	cout<<c<<" ";
+	cout<<ans;
+	return 0;
+}
