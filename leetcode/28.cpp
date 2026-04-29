29305c6fcae30392118806c88a04371ca7a698e4
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-04-29 11:28:21 +0530

    .

--- leetcode/28.cpp
+++ leetcode/28.cpp
@@ -4,6 +4,8 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        if(needle.size() > haystack.size()) return -1;
+        
         for(int i=0; i<=haystack.size()-needle.size(); i++){
             if(haystack.substr(i, needle.size())==needle){
                 return i;
