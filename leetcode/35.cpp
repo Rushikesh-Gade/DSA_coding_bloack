8e2ef00bbd80cce4c44d12782d3ea962afa925ea
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-04-29 11:59:30 +0530

    .

--- leetcode/35.cpp
+++ leetcode/35.cpp
@@ -4,13 +4,14 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for(int i=0;i<=nums.size();i++){
-            if(nums[i]==target){
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
                 return i;
             }
-            else if(nums[i]<target-1){
-                return i+1;
+            if (nums[i] > target) {
+                return i;
             }
-        }      
+        }
+        return nums.size();
     }
 };
