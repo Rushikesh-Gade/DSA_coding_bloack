be76f7dca7fe3d0bac1b115a8f27713966e3f874
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-05-02 00:04:32 +0530

    .

--- leetcode/350.cpp
+++ leetcode/350.cpp
@@ -1,12 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        multi_set<int>pahila (nums1.begin(),nums1.end());
-        multi_set<int>dusra (nums2.begin(),nums2.end());
+        multiset<int>pahila (nums1.begin(),nums1.end());
+        multiset<int>dusra (nums2.begin(),nums2.end());
         vector<int>uttar;
-        multi_set_intersect(pahila.begin(),pahila.end(),
+        set_intersection(pahila.begin(),pahila.end(),
                             dusra.begin(),dusra.end(),
-                            back_inersect(uttar));
+                            back_inserter(uttar));
         return uttar;
     }
 };
