9e345f0e088d73ab3e21d3afedfd189aa5350578
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-05-12 02:09:03 +0530

    88

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -13,13 +13,13 @@ public:
         sort(nums1.begin(),nums1.end());
     }
 };
-int main(){
-    Solution s;
-    vector<int> nums1 = {1,2,3,0,0,0};
-    vector<int> nums2 = {2,5,6};
-    s.merge(nums1,3,nums2,3);
-    for (int i = 0; i<nums1.size(); i++){
-        cout<<nums1[i]<<" ";
-    }
-    return 0;
-}
+// int main(){
+//     Solution s;
+//     vector<int> nums1 = {1,2,3,0,0,0};
+//     vector<int> nums2 = {2,5,6};
+//     s.merge(nums1,3,nums2,3);
+//     for (int i = 0; i<nums1.size(); i++){
+//         cout<<nums1[i]<<" ";
+//     }
+//     return 0;
+// }
