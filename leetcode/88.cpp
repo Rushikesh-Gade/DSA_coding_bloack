43ab3b0c4b22eef7b1c7182af17ea2f329191ca0
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-05-12 02:08:32 +0530

    88

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -9,3 +13,13 @@ public:
         sort(nums1.begin(),nums1.end());
     }
 };
+int main(){
+    Solution s;
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    s.merge(nums1,3,nums2,3);
+    for (int i = 0; i<nums1.size(); i++){
+        cout<<nums1[i]<<" ";
+    }
+    return 0;
+}
