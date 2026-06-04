94a8529c15f1fe8f4743ccc067b3748990420655
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-06-04 11:07:16 +0530

    274

---
+++ leetcode/274.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int s=0;
+        int e=n-1;
+        int ans=0;
+        sort(citations.begin(),citations.end());
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(citations[m]>=n-m){
+                ans=max(ans,n-m);
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        return ans;
+
+    }
+};
