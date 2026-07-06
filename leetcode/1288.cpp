0e3ead3ba8e46afb7f87ec76d1d5379602746365
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-07-06 10:59:20 +0530

    1288

---
+++ leetcode/1288.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& q) {
+        sort(q.begin(),q.end(),[](vector<int>&a,vector<int>&b){
+            if(a[0]!=b[0]){
+                return a[0]<b[0];
+            }
+            return a[1]>b[1];
+        });
+        int c=0, right=0;
+        for(auto i:q){
+            if(i[1]>right){
+                c++;
+                right=i[1];
+            }
+        }
+        return c;
+    }
+};
