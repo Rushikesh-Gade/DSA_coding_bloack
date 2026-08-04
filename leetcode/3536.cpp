dfb2296d7d97b9e8c77122f990eece1c1c9c5108
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-04 15:00:56 +0530

    3536

---
+++ leetcode/3536.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int f=0;
+        int s=0;
+        while(n>0){
+            int d=n%10;
+            if(d>=f){
+                s=f;
+                f=d;
+            }else if(s<d){
+               s=d; 
+            }
+            n/=10;
+        }
+        return f*s;
+    }
+};
