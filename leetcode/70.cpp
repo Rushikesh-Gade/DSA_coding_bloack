951486445a5416d349b85f596c8faaf62112e03a
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-08 00:18:50 +0530

    70

--- leetcode/70.cpp
+++ leetcode/70.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
-using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        if (n <= 2) return n;
+        
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
         }
-        return climbStairs(n-1) + climbStairs(n-2);
-        // return n;
+        return second;
     }
 };
