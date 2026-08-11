d42eb7a31807b9e49f6d3528b01606a7abaf9c00
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-11 14:32:46 +0530

    41

---
+++ leetcode/41.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1;i<=n+1;i++){
+            if(s.find(i)==s.end()){
+                return i;
+            }
+        }
+        return n+1;
+    }
+};
