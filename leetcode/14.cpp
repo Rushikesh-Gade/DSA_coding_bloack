c50c00337bc43c9ff11c5a24839e01d31f71ddc0
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-11 13:01:08 +0530

    14

--- leetcode/14.cpp
+++ leetcode/14.cpp
@@ -6,7 +6,7 @@ public:
         string ans="";
         int n=strs.size();
         string first=strs[0],last=strs[n-1];
-        for(int i=0;i<strs.size();i++){
+        for(int i=0;i<first.length();i++){
             if (first[i]==last[i]){
                 ans += first[i];
             } 
