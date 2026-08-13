f64259a1c4be0292c3b6d41e4b120a43f4f08bc6
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-13 12:29:30 +0530

    34

---
+++ leetcode/34.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+    int n = nums.size();
+    int left = 0, right = n - 1;
+    int first = -1, last = -1;
+
+    // Find first occurrence
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] >= target) right = mid - 1;
+        else left = mid + 1;
+        if (nums[mid] == target) first = mid;
+    }
+
+    left = 0; right = n - 1;
+    // Find last occurrence
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] <= target) left = mid + 1;
+        else right = mid - 1;
+        if (nums[mid] == target) last = mid;
+    }
+
+    return {first, last};
+}
+
+};
