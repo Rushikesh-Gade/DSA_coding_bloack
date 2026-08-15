6fb4d5f6633f4150b754c9ef40001a6ae4bd1348
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-15 13:16:49 +0530

    36

--- leetcode/36.cpp
+++ leetcode/36.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i<9; i++){
             for(int j = 0;j<9; j++){
                 char c = board[i][j];
-                if(c=='0')continue;
+                if(c=='.')continue;
                 int boxidx = (i/3)*3 + (j/3);
                 if(row[i].count(c) || cols[j].count(c) || boxes[boxidx].count(c)) return false;
                 row[i].insert(c);
