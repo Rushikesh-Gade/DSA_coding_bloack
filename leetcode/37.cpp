0a16ff649c55e32c4fdda0356a6eb129bb39e289
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-08-15 14:03:58 +0530

    37

---
+++ leetcode/37.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        backtrack(board);
+    }
+    bool backtrack(vector<vector<char>> & board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1'; c<='9';c++){
+                        if(isvalid(board,i,j,c)){
+                            board[i][j]=c;
+                            if(backtrack(board)) return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isvalid(vector<vector<char>>& board, int row, int col, char c){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
+            int boxrow=3*(row/3)+i/3;
+            int boxcol=3*(col/3)+i%3;
+            if(board[boxrow][boxcol]==c) return false;
+        }
+        return true;
+    }
+};
