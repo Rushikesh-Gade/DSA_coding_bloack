167814db96847bf463c6e2cbae7bf47bb738c340
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-09-21 11:55:12 +0530

    l

--- linkedlist/adress2.cpp
+++ linkedlist/adress2.cpp
@@ -9,12 +9,17 @@ class Node {
 int main() {
     Node* first = new Node();
     first->data = 100;
-    first->next = NULL;
+    // first->next = NULL;
     Node* second = new Node();
     second->data = 200;
-    second->next = NULL;
+    // second->next = NULL;
     first->next = second;
+    Node* third = new Node();
+    third->data = 300;
+    third->next = NULL;
+    second->next = third;
     cout<< first->data << endl;
     cout<< second->data << endl;
+    cout<< third->data << endl;
     return 0;
 }
